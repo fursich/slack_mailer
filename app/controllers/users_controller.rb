abe18bb7f6ab2261428d5c34b8fd34b44c807046
Koji Onishi <fursich0@gmail.com>
2017-12-13 11:34:51 +0900

    表示系の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      redirect_back(fallback_location: users_path) unless @user == current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
